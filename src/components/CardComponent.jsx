5347172d8257925be873061f1867b5bae4b8c08f
simanto848 <simanto848@email.com>
2024-06-16 00:14:42 +0600

    create a card component

---
+++ src/components/CardComponent.jsx
@@ -0,0 +1,30 @@
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card } from "antd";
+const { Meta } = Card;
+const CardComponent = () => (
+  <Card
+    className="w-[300px]"
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <SettingOutlined key="setting" />,
+      <EditOutlined key="edit" />,
+      <EllipsisOutlined key="ellipsis" />,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
+);
+export default CardComponent;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import Faculties from "./Admin/Faculties/Faculties";
+import CardComponent from "../components/CardComponent";
 
 export default function Dashboard() {
-  const location = useLocation();
-  const [tab, setTab] = useState("");
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const tabFormUrl = searchParams.get("tab");
-    if (tabFormUrl) {
-      setTab(tabFormUrl);
-    }
-  }, [location.search]);
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      <div>
-        <h1>Welcome to Dashboard</h1>
-        {/* Faculties... */}
-        {tab === "faculties" && <Faculties />}
+    <div className="min-h-screen flex flex-col items-start justify-start">
+      <div className="flex flex-wrap justify-start gap-4">
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
       </div>
     </div>
   );
